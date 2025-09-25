ada4f91d8597dbca7ffea2ef0ce9412ad55f67be
DavidBjerreBjoerklund <DavidBjerreBjoerklund@users.noreply.github.com>
2025-09-25 16:33:37 +0200

    Expose commands package for Rhino plug-in

---
+++ src/plugin/dev/RhinoToSlicer/commands/__init__.py
@@ -0,0 +1,19 @@
+"""Command helpers exposed by the RhinoToSlicer package."""
+
+from __future__ import print_function
+
+from .slice import (
+    COMMAND_NAME,
+    RunCommand,
+    send_to_prusaslicer,
+    send_to_slicer,
+    set_prusaslicer_path,
+)
+
+__all__ = [
+    "COMMAND_NAME",
+    "RunCommand",
+    "send_to_prusaslicer",
+    "send_to_slicer",
+    "set_prusaslicer_path",
+]
